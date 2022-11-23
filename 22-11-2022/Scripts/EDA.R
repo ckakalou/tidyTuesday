da3d7121710e2f0acad06af92195a967766eadc4
ckakalou <ckakalou@gmail.com>
2022-11-23 22:18:19 +0200

    eda started

--- 22-11-2022/Scripts/EDA.R
+++ 22-11-2022/Scripts/EDA.R
@@ -1,16 +1,3 @@
-# Get the Data
 
-# Read in with tidytuesdayR package 
-# Install from CRAN via: install.packages("tidytuesdayR")
-# This loads the readme and all the datasets for the week of interest
-
-# Either ISO-8601 date or year/week works!
-
-tuesdata <- tidytuesdayR::tt_load('2022-11-22')
-tuesdata <- tidytuesdayR::tt_load(2022, week = 47)
-
-museums <- tuesdata$museums
-
-# Or read in the data manually
-
-# museums <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-22/museums.csv')
+# Find unique geodemographic group
+geoDemoGroups <- unique(museums$Area_Geodemographic_group) %>% data.frame()
