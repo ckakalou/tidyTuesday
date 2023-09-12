83e07feb8a205ff76e73e77f76a6c225440f26a5
ckakalou <ckakalou@certh.gr>
2023-09-12 16:51:31 +0300

    union scripts

---
+++ 05-09-2023/Scripts/app.R
@@ -0,0 +1,4 @@
+demographics <- readr::read_csv('05-09-2023/Data/demographics.csv' )
+wages <- readr::read_csv('05-09-2023/Data/wages.csv')
+states <- readr::read_csv('05-09-2023/Data/states.csv')
+
